0a20daa39430a9f02e7639723041c9230887ebdf
zhouwei <zhou21192345@163.com>
2021-09-21 22:44:13 +0800

    first commit

---
+++ src/pages/BasicList/index.tsx
@@ -0,0 +1,84 @@
+import { PageContainer } from '@ant-design/pro-layout';
+import { Col, Row, Table, Card, Button, Pagination, Space } from 'antd';
+import styles from './index.less';
+
+const index = () => {
+    const dataSource = [
+        {
+            key: '1',
+            name: '胡彦斌',
+            age: 32,
+            address: '西湖区湖底公园1号',
+        },
+        {
+            key: '2',
+            name: '胡彦祖',
+            age: 42,
+            address: '西湖区湖底公园1号',
+        },
+    ];
+
+    const columns = [
+        {
+            title: '姓名',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: '年龄',
+            dataIndex: 'age',
+            key: 'age',
+        },
+        {
+            title: '住址',
+            dataIndex: 'address',
+            key: 'address',
+        },
+    ];
+
+    const searchLayout = () => {};
+
+    const beforeTableLayout = () => {
+        return (
+            <Row>
+                <Col xs={24} sm={12}>
+                    ...
+                </Col>
+                <Col xs={24} sm={12} className={styles.tableToolbar}>
+                    <Space>
+                        <Button type="primary">Add</Button>
+                        <Button type="primary">Add2</Button>
+                    </Space>
+                </Col>
+            </Row>
+        );
+    };
+
+    const afterTableLayout = () => {
+        return (
+            <Row>
+                <Col xs={24} sm={12}>
+                    ...
+                </Col>
+                <Col xs={24} sm={12} className={styles.tableToolbar}>
+                    <Pagination />
+                </Col>
+            </Row>
+        );
+    };
+
+    return (
+        <PageContainer>
+            {searchLayout()}
+            <Card>
+                {beforeTableLayout()}
+                <Table dataSource={dataSource} columns={columns} pagination={false} />
+                {afterTableLayout()}
+            </Card>
+        </PageContainer>
+    );
+};
+
+export default index;
+
+// https://www.yuque.com/aspirantzhang/antdprov5/fxn0s9
